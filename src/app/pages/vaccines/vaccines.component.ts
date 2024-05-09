bf1ffd9745f259a748309de14dc7b85e9eb925c3
Gabiroba <gabrielcardosogirarde@gmail.com>
2024-05-09 05:50:13 -0300

    refactor: Refatorando tela de login

--- src/app/pages/vaccines/vaccines.component.ts
+++ src/app/pages/vaccines/vaccines.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { VaccineTableComponent } from './components/vaccine-table/vaccine-table.component';
 import { PaginatorComponent } from '../../components/paginator/paginator.component';
+import { FilterComponent } from '../../components/filter/filter.component';
 
 @Component({
   selector: 'app-vaccines',
   standalone: true,
-  imports: [VaccineTableComponent, PaginatorComponent],
+  imports: [VaccineTableComponent, PaginatorComponent, FilterComponent],
   templateUrl: './vaccines.component.html',
   styleUrl: './vaccines.component.scss'
 })
-export default class VaccinesComponent { }
+export default class VaccinesComponent {
+
+  openFilterModal = signal(false);
+}
+
