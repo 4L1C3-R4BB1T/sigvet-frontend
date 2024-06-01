dd85370f146d5622854088b137577667e8768f65
Livia <livinha.guimaraes.013@gmail.com>
2024-06-01 09:40:50 -0300

    fix: Ajustando dashboard status com toFixed(2)

--- src/app/pages/dashboard/components/dashboard-status/dashboard-status.component.ts
+++ src/app/pages/dashboard/components/dashboard-status/dashboard-status.component.ts
@@ -25,7 +25,7 @@ export class DashboardStatusComponent implements OnInit {
     if (previous === 0) {
       return current > 0 ? 100 : 0;
     }
-    return ((current - previous) / previous) * 100;
+    return parseFloat((((current - previous) / previous) * 100).toFixed(2));
   }
 
   calcPercentForClients() {
